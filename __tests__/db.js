eeb4014fb7ebb4d73d2f1dffb310d1f982647bbe
Nachiket Pingle <Nachiket1@users.noreply.github.com>
2021-04-07 16:13:49 -0700

    db tests ongoing

--- __tests__/db.js
+++ __tests__/db.js
@@ -1,50 +1,48 @@
 // Here we will be unit testing the main database functions from server/models/sickBayModels.js
 const fs = require('fs');
 const path = require('path');
-const db = require('../models/sickBayModels.js');
-
-const testJsonFile = path.resolve(__dirname, '../models/sickBayModels.test.json');
+const mongoose = require('mongoose');
+const SickBay = require('../server/models/sickBayModels');
+const dbName = 'sickBay';
 
 
 /**
  * Using Jest -- "describe" to test sections separately and to have readable outputs
  */
 
-xdescribe('db unit tests', () => {
+describe('db unit tests', () => {
     /**
      * Jest runs the "beforeAll" function once, before any tests are executed.
      * The tests won't start until the "database" has been reset to an empty Array!
      */
-    beforeAll((done) => {
-      fs.writeFile(testJsonFile, JSON.stringify([]), () => {
-        db.reset();
-        done();
-      });
-    });
-  
-    afterAll((done) => {
-      fs.writeFile(testJsonFile, JSON.stringify([]), done);
+    beforeAll( async () => {
+      await mongoose.connect(`mongodb://localhost:3000/${dbName}`, {useNewUrlParser: true});
     });
 
     // 1. Testing Schema
-    it('returns an error when Title is not provided'), () => {
-    
-    }
+    it('returns an error when Title is not provided', async (done) => {
+        const user = await SickBay.findOne({ username: 'Goobly5' });
+        expect(user.username).toBeTruthy();
+        done();
+    })
 
-    it('returns an error when Title is not a string'), () => {
+    afterAll( async () => {
+        await mongoose.connection.close()
+    })
 
-    }
+    // it('returns an error when Title is not a string'), () => {
 
-    it('returns an error when Description is not a string'), () => {
+    // }
 
-    }
+    // it('returns an error when Description is not a string'), () => {
 
-    it('returns an error when Category is not a string'), () => {
+    // }
 
-    }
+    // it('returns an error when Category is not a string'), () => {
 
-    it('returns an error when ImageURL is not a string'), () => {
+    // }
 
-    }
-}
+    // it('returns an error when ImageURL is not a string'), () => {
 
+    // }
+})
