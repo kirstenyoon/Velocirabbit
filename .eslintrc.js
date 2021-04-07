e7c384ecaf6edcc881079ea6539bf3e20137cdc2
Peter (Hyesung) Lee <hyesunglee91@gmail.com>
2021-04-07 14:19:24 -0700

    basic puppeteer test done

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,15 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  plugins: ['react'],
+  rules: {},
 };
--- __tests__/enzyme.js
+++ __tests__/enzyme.js
@@ -6,70 +6,72 @@ import { createShallow } from '@material-ui/core/test-utils';
 
 // Enzyme is a wrapper around React test utilities which makes it easier to
 // shallow render and traverse the shallow rendered tree.
+import { Box } from '@material-ui/core';
 import MainBody from '../client/components/mainBody.jsx';
 import Navbar from '../client/components/navbar.jsx';
-import { Box } from '@material-ui/core';
 
 // Newer Enzyme versions require an adapter to a particular version of React
 configure({ adapter: new Adapter() });
 
 describe('React unit tests', () => {
-	describe('MainBody', () => {
-		let wrapper;
-		const props = {};
+  describe('MainBody', () => {
+    let wrapper;
+    const props = {};
 
-		beforeAll(() => {
-			wrapper = shallow(<MainBody {...props} />);
-		});
+    beforeAll(() => {
+      wrapper = shallow(<MainBody {...props} />);
+    });
 
-		it('Renders a <div> with an instance of <ItemOuterContainer/>', () => {
-			expect(wrapper.type()).toEqual('div');
-		});
-	});
+    it('Renders a <div> with an instance of <ItemOuterContainer/>', () => {
+      expect(wrapper.type()).toEqual('div');
+    });
+  });
 
-	xdescribe('Box from MaterialUI', () => {
-		let wrapper;
-		const props = {};
+  xdescribe('Box from MaterialUI', () => {
+    let wrapper;
+    const props = {};
 
-		beforeAll(() => {
-			wrapper = createShallow(<MainBody {...props} />);
-		});
+    beforeAll(() => {
+      wrapper = createShallow(<MainBody {...props} />);
+    });
 
-		xit('Renders a Box from MaterialUI', () => {});
-	});
+    xit('Renders a Box from MaterialUI', () => {});
+  });
 
-	describe('MaterialUI Components', () => {
-		describe('Product description is displayed in <Box>', () => {
-			let wrapper;
-			const props = {
-				title: 'Face Shield',
-			};
+  describe('MaterialUI Components', () => {
+    describe('Product description is displayed in <Box>', () => {
+      let wrapper;
+      const props = {
+        title: 'Face Shield',
+      };
 
-			beforeAll(() => {
-				wrapper = shallow(
-					<Box component="span" {...props}>
-						Product: {props.title}
-					</Box>
-				);
-			});
+      beforeAll(() => {
+        wrapper = shallow(
+          <Box component="span" {...props}>
+            Product:
+            {' '}
+            {props.title}
+          </Box>,
+        );
+      });
 
-			it('Renders a <span> with product label and description', () => {
-				expect(wrapper.type()).toEqual('span');
-				expect(wrapper.text()).toEqual('Product: Face Shield');
-				// check color
-				// expect(wrapper.find("strong").text()).toMatch("Mega")
-			});
-		});
+      it('Renders a <span> with product label and description', () => {
+        expect(wrapper.type()).toEqual('span');
+        expect(wrapper.text()).toEqual('Product: Face Shield');
+        // check color
+        // expect(wrapper.find("strong").text()).toMatch("Mega")
+      });
+    });
 
-		xdescribe('Box from MaterialUI', () => {
-			let wrapper;
-			const props = {};
+    xdescribe('Box from MaterialUI', () => {
+      let wrapper;
+      const props = {};
 
-			beforeAll(() => {
-				wrapper = createShallow(<MainBody {...props} />);
-			});
+      beforeAll(() => {
+        wrapper = createShallow(<MainBody {...props} />);
+      });
 
-			xit('Renders a Box from MaterialUI', () => {});
-		});
-	});
+      xit('Renders a Box from MaterialUI', () => {});
+    });
+  });
 });
