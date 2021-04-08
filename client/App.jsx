d1b3880367a51f8745598536e8d01d03c6394f44
Peter (Hyesung) Lee <hyesunglee91@gmail.com>
2021-04-07 20:43:49 -0700

    rsetting up react redirect routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './components/body/Home';
 import LoggedIn from './components/body/LoggedIn';
 import Admin from './components/body/Admin';
 
 function App() {
+  // useEffect(() => {
+  //   fetch(`/ + ${username} + ${password}`);
+  // });
+  const [user, setUser] = useState('admin');
+
+  // setUser('user');
+  // setUser("admin")
+
   return (
     <div className="App">
       <main>
+        {user === 'user' && <Redirect to="/loggedIn" />}
+        {user === 'admin' && <Redirect to="/admin" />}
         <Switch>
-          <Route path="/" component={Home} exact />
+          <Route exact path="/" component={Home} />
           <Route path="/loggedIn" component={LoggedIn} />
           <Route path="/admin" component={Admin} />
         </Switch>
