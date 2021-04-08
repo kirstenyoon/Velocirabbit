34ba203e827b030fe06f9330fb6668bc6988a2e9
Nachiket Pingle <Nachiket1@users.noreply.github.com>
2021-04-08 10:59:03 -0700

    userController updated username is now hashed & stored in hashed id

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const api = require('./routes/api');
 // ** New feature require
 // require express-session to store user verification information in a session that will be sent in a cookie
-const session = require('express-session');
+// const session = require('express-session');
 
 // Since fetch was unable with an endpoint, used cors to fetch with an entire url
 app.use(cors());
@@ -15,13 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 
 // ** New feature for creating a session
 // This will immediately create a session with null value. It will be sent to the client when they access our site
-app.use(session({
-	secret: 'wooblify', // secret taken from ----- to be implemented!
-	cookie: {
-		httpOnly: true, // make it http only
-		// maxAge: 1 * 60 * 60 * 24 * 1000 // max age of 2 hours, if this is not set then the session will be destroyed on a condition like exiting the browser
-	}
-}))
+// app.use(session({
+// 	secret: 'wooblify', // secret taken from ----- to be implemented!
+// 	cookie: {
+// 		httpOnly: true, // make it http only
+// 		// maxAge: 1 * 60 * 60 * 24 * 1000 // max age of 2 hours, if this is not set then the session will be destroyed on a condition like exiting the browser
+// 	}
+// }))
 
 app.use('/api', api);
 
