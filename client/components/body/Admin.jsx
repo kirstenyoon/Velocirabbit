bab83e2202bef3f6993407fc4015b5d3fd7d3b1c
Kirsten Yoon <kirstenyoon@gmail.com>
2021-04-08 20:06:16 -0700

    added admin console, logout button

--- client/components/body/Admin.jsx
+++ client/components/body/Admin.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 function Admin() {
 	return (
 		<div className="admin">
-			<h1>YAY! Admin!!</h1>
+			<h1>Admin Console</h1>
+			<div>
+				<form action="/api/allUsers">
+					<button>GET ALL USERS</button>
+				</form>
+				<br></br>
+				<form action="/api/allProducts">
+					<button>GET ALL PRODUCTS</button>
+				</form>
+				<br></br>
+				<br></br>
+				<form action="/api/checkSession">
+					<button>CHECK SESSION</button>
+				</form>
+				<br></br>
+				<form action="/api/logout">
+					<button>LOGOUT</button>
+				</form>
+			</div>
 		</div>
 	);
 }
--- client/components/body/LoggedIn.jsx
+++ client/components/body/LoggedIn.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 function LoggedIn() {
-  return (
-    <div className="userLoginPage">
-      <h1>Welcome back, user!!</h1>
-    </div>
-  );
+	return (
+		<div className="userLoginPage">
+			<h1>Welcome back!</h1>
+			<form action="/api/logout">
+				<button>LOGOUT</button>
+			</form>
+		</div>
+	);
 }
 
 export default LoggedIn;
